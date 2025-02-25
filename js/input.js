c00fa5eca3cddd2d94e6918d9a9ea08be6f51ded
mdrasel97 <mollarasel972@gmail.com>
2025-02-25 15:37:26 +0600

    remember added

--- js/input.js
+++ js/input.js
@@ -4,6 +4,21 @@ document.getElementById('btn-login')
 .addEventListener('click', function(event){
     event.preventDefault();
     const mobileNumber = document.getElementById('mobile-number').value;
+    const convertedNumber = parseInt(mobileNumber);
     const pin = document.getElementById('pin').value;
-    console.log(mobileNumber, pin);
+    const convertedPin = parseInt(pin);
+    
+    // console.log(typeof convertedPin, typeof convertedNumber)
+    if(mobileNumber.length === 11 && mobileNumber.startsWith("01")){
+        console.log('mobileNumber')
+        if(convertedPin === 1234){
+            window.location.href="main.html";
+        }
+        else{
+            alert('Noooo, this pin is invalid')
+        }
+    }
+    else{
+        alert('please a valid Number input')
+    }
 })
