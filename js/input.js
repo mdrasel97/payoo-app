f889af57b27c12094d11d4c284b3b328d81b06ae
mdrasel97 <mollarasel972@gmail.com>
2025-02-25 13:50:02 +0600

    login button customize

--- js/input.js
+++ js/input.js
@@ -1,1 +1,9 @@
 // alert('Hello, world?');
+
+document.getElementById('btn-login')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    const mobileNumber = document.getElementById('mobile-number').value;
+    const pin = document.getElementById('pin').value;
+    console.log(mobileNumber, pin);
+})
